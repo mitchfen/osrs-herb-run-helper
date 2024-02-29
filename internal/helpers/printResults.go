71681f8c3be264007875684e2b1ce1028562b709
Mitchell Fenner <mitchfen@protonmail.com>
2024-02-29 08:26:28 -0500

    feat: round results

--- internal/helpers/printResults.go
+++ internal/helpers/printResults.go
@@ -5,11 +5,9 @@ import (
 )
 
 func PrintResults(herbs []Herb) {
-	fmt.Printf("If you print %v seeds in %v patches, and assume %v herbs per patch. Then the expected profits are as follows:\n",
+	fmt.Printf("If you print %v seeds in %v patches, and assume %v herbs per patch.\nThen the expected profits are as follows:\n",
 		numberofPatches, numberofPatches, numberofHerbsPerSeed)
 	for i := 0; i < len(herbs); i++ {
-		//fmt.Printf("%v herb price: %v\n", herbs[i].Name, herbs[i].HerbPrice)
-		//fmt.Printf("%v seed price: %v\n", herbs[i].Name, herbs[i].SeedPrice)
-		fmt.Printf("%v: %d\n", herbs[i].Name, int(herbs[i].ExpectedProfit))
+		fmt.Printf("%v: %dk\n", herbs[i].Name, int(herbs[i].ExpectedProfit/1000.0))
 	}
 }
