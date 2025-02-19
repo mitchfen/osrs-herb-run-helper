b761cc8fa29165ae42d74e6b7d6f8e9f98ef0698
Mitchell Fenner <mfenner@configit.com>
2025-02-19 18:47:12 -0500

    fix text and screenshot

--- internal/helpers/printResults.go
+++ internal/helpers/printResults.go
@@ -5,7 +5,7 @@ import (
 )
 
 func PrintResults(herbs []Herb) {
-	fmt.Printf("If you print %v seeds in %v patches, and assume %v herbs per patch.\nThen the expected profits are as follows:\n",
+	fmt.Printf("Assuming you plant %v seeds in %v patches, and assuming %v herbs per patch.\nThen the expected profits are as follows:\n",
 		numberofPatches, numberofPatches, numberofHerbsPerSeed)
 	for i := 0; i < len(herbs); i++ {
 		fmt.Printf("%v: %dk\n", herbs[i].Name, int(herbs[i].ExpectedProfit/1000.0))
